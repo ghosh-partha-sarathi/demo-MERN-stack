56f007790ae90a83bbe02b3382291969f3b850f7
ghosh-partha-sarathi <partha187@gmail.com>
2022-01-09 17:53:11 +0000

    index.js file replaced with index.jsx

---
+++ src/app/index.jsx
@@ -0,0 +1,8 @@
+import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Dashboard } from "./components/Dashboard";
+import { Main } from "./components/Main";
+
+// ReactDOM.render(<Dashboard />, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"));
